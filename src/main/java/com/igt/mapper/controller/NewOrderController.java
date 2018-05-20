ad14d063844128a74b8f2723b36cde6725ab361f
ICH <@test>
2018-05-20 19:18:55 +0200

    Beziehungen fertig

---
+++ src/main/java/com/igt/mapper/controller/NewOrderController.java
@@ -0,0 +1,236 @@
+package com.igt.mapper.controller;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.igt.mapper.Config;
+import com.igt.mapper.DatabaseController;
+import com.igt.mapper.model.Company;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.transaction.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/company")
+public class CompanyController {
+
+
+    @RequestMapping(value="/update", method = RequestMethod.PUT)
+    public void updateCompany(Company c) {
+        EntityManagerFactory emf = DatabaseController.emf;
+        TransactionManager tm = DatabaseController.tm;
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+
+            Company companyToUpdate = em.find(Company.class, c.getC_ID());
+            companyToUpdate = c;
+
+            em.merge(c);
+
+            em.flush();
+            em.close();
+            tm.commit();
+
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @RequestMapping(value="/get/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    Company getCompany(@PathVariable(value = "id", required = true) String id) {
+        EntityManagerFactory emf = DatabaseController.emf;
+        TransactionManager tm = DatabaseController.tm;
+        Company comp = null;
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            tm.begin();
+
+            comp = em.find(Company.class, id);
+
+            em.flush();
+            em.close();
+            tm.commit();
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        }
+        return comp;
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public @ResponseBody
+    void createCompany(@RequestParam(value ="name", required = true) String name) {
+        EntityManagerFactory emf = DatabaseController.emf;
+        TransactionManager tm = DatabaseController.tm;
+        Company company1 = new Company();
+
+        company1.setC_NAME(name);
+
+        try {
+
+            EntityManager em = emf.createEntityManager();
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+            em.persist(company1);
+            em.flush();
+            em.close();
+            tm.commit();
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @RequestMapping(value="/delete", method = RequestMethod.GET)
+    public @ResponseBody
+    void deleteCompany(@RequestParam(value = "id") String id){
+        EntityManagerFactory emf = DatabaseController.emf;
+        TransactionManager tm = DatabaseController.tm;
+        try {
+            EntityManager em = emf.createEntityManager();
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+
+            Company comp = em.find(Company.class, id);
+
+            em.remove(comp);
+
+            em.flush();
+            em.close();
+            tm.commit();
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @RequestMapping(value="/getAllIds", method = RequestMethod.GET)
+    public @ResponseBody
+    List<String> getCompanyIds(){
+        EntityManagerFactory emf = DatabaseController.emf;
+        TransactionManager tm = DatabaseController.tm;
+        List<Company> allCompanys = new ArrayList<Company>();
+        List<String> companyIdz = new ArrayList<String>();
+
+        try {
+            EntityManager em = emf.createEntityManager();
+
+            String queryString = new String("SELECT c FROM Company c");
+
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+
+            Query q = em.createQuery(queryString);
+            allCompanys = q.getResultList();
+
+            for(Company c : allCompanys){
+                companyIdz.add(c.getC_ID());
+            }
+
+            em.flush();
+            em.close();
+            tm.commit();
+
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        }
+
+        return companyIdz;
+    }
+
+    @RequestMapping(value="/getAll", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Company> getCompanys(){
+        EntityManagerFactory emf = DatabaseController.emf;
+        TransactionManager tm = DatabaseController.tm;
+        List<Company> allCompanys = new ArrayList<Company>();
+
+
+        try {
+            EntityManager em = emf.createEntityManager();
+
+            String queryString = new String("SELECT c FROM Company c");
+
+            tm.setTransactionTimeout(Config.TRANSACTION_TIMEOUT);
+            tm.begin();
+
+            Query q = em.createQuery(queryString);
+            allCompanys = q.getResultList();
+
+            em.flush();
+            em.close();
+            tm.commit();
+
+        } catch (NotSupportedException e) {
+            e.printStackTrace();
+        } catch (SystemException e) {
+            e.printStackTrace();
+        } catch (HeuristicMixedException e) {
+            e.printStackTrace();
+        } catch (HeuristicRollbackException e) {
+            e.printStackTrace();
+        } catch (RollbackException e) {
+            e.printStackTrace();
+        }
+
+        return allCompanys;
+    }
+
+}
---
+++ src/main/java/com/igt/mapper/model/NewOrder.java
@@ -0,0 +1,48 @@
+package com.igt.mapper.model;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.id.IdentifierGenerator;
+import org.hibernate.search.annotations.Indexed;
+
+import javax.annotation.Generated;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+
+@Entity
+@Indexed
+@Table(name = "COMPANY")
+public class Company  implements Serializable{
+
+    @Id
+    @GeneratedValue(generator="uuid")
+    @GenericGenerator(name="uuid", strategy = "uuid2")
+    @Column(name="C_ID", updatable = false, nullable = false)
+    private String C_ID;
+    public String getC_ID() {
+        return C_ID;
+    }
+    public void setC_ID(String c_ID) {
+        C_ID = c_ID;
+    }
+
+
+    @Column
+    private String C_NAME;
+    public String getC_NAME() {
+        return C_NAME;
+    }
+    public void setC_NAME(String c_NAME) {
+        C_NAME = c_NAME;
+    }
+
+
+    public Company()
+    {
+    }
+
+
+
+
+}
